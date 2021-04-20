f02e17ff440b707da610ffb15eed4cb9b1127da4
liviulenter <lenterliviu@gmail.com>
2021-04-20 11:46:23 +0200

    second commit

--- src/main/java/casino/controllers/UserController.java
+++ src/main/java/casino/controllers/UserController.java
@@ -4,14 +4,12 @@ package casino.controllers;
 import casino.entities.User;
 import casino.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/user")
+@RestController // #RestController, @Controller - delcaram clasa ca fiind bean in Spring
+@RequestMapping("/user") // mapare a controller-ului pe path-ul /user
 //HTTP methods: GET, POST, PUT, DELETE
 //GET - get = aducem resurse (luam resurse/date de pe server)
 //POST - create/save = cream sau salvam
@@ -22,25 +20,34 @@ public class UserController {
     @Autowired
     UserService userService;
 
-    @GetMapping("/findById") // localhost:8080/user/findById
-    public User findById(Long userId) {
+    @GetMapping("/findById/{id}") // http://localhost:8080/user/findById/2
+//    @RequestMapping(
+//            method = {RequestMethod.GET}
+//    )
+    //mapare a metodei din controller pe path-ul /findById => path-ul final: /user/findById
+    //specificarea methodei din HTTP folosita pe acest path
+    public User findById(@PathVariable Long userId) {
         return userService.findById(userId);
     }
 
+    @GetMapping("/findAll") // http://localhost:8080/user/findAll
     public List<User> findAll() {
         return userService.findAll();
 
     }
 
-    public User save(User user) {
+    @PostMapping("/save")
+    public User save(@RequestBody User user) {
         return userService.save(user);
     }
 
-    public void delete(Long userId) {
+    @DeleteMapping("/delete/{id}")
+    public void delete(@PathVariable Long userId) {
         userService.delete(userId);
     }
 
-    public User findByUsername(String username) {
+    @GetMapping("/findByUsername/{username}")
+    public User findByUsername(@PathVariable String username) {
         return userService.findByUsername(username);
     }
 
